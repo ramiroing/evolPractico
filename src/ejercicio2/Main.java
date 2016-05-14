62ca719edb2ee803146f3185018f94833c8e891b
ramiroing <ramiroing@gmail.com>
2016-05-14 19:34:27 -0300

    lector del archivo de instancia

--- src/ejercicio2/Main.java
+++ src/ejercicio2/Main.java
@@ -1,9 +1,50 @@
 package ejercicio2;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
+	
+	
+	static Double[][] LeerInstancia(String archivo) throws FileNotFoundException, IOException {
+	      String cadena;
+	      FileReader f = new FileReader(archivo);
+	      BufferedReader b = new BufferedReader(f);
+	      ArrayList<Double[]> lines = new ArrayList<Double[]>();
+	      while((cadena = b.readLine()) != null) {
+	    	  String[] parts = cadena.split(" ");
+	    	  Double[] values = new Double[parts.length];
+	    	  for ( int i=0 ; i < parts.length ; i++ )
+	    		  values[i] = Double.parseDouble(parts[i]);
+	    	  lines.add(values);
+	      }
+	      b.close();
+	      f.close();
+	      return lines.toArray( new Double[lines.size()][] );
+	}
+	
+	
 	public static void main(String[] args) {
-		int a = 2;
+		
+		try {
+			Double[][] DATA_INSTANCE = LeerInstancia("ejemplo.txt");
+			
+			System.out.println(DATA_INSTANCE[1][1]);
+			
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
